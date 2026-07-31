8beb4a376e23a7dd1091209f2c8af3bdc4a5a5e5
susan339 <1123696584@qq.com>
2026-07-31 10:40:00 +0800

    Update page.js

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,9 +1,8 @@
 "use client";
-import React, { use } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
-export default function ProductDetailPage({ params: paramsPromise }) {
-  const params = use(paramsPromise);
+export default function ProductDetailPage({ params }) {
   const id = params.id;
 
   const localProducts = {
