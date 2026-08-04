7c08a69213adfbd89d082a3198e37a56e78d9284
susan339 <1123696584@qq.com>
2026-08-04 18:01:01 +0800

    Update page.js

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -5,212 +5,37 @@ import Link from 'next/link';
 
 export default function ProductsPage() {
   const products = [
-  {
-    "slug": "hd12nl400c",
-    "title": "HD12NL400C 12\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/Hfdeb7c97246f4c289e9d539223ba06fdH.jpg"
-  },
-  {
-    "slug": "hd10n75",
-    "title": "HD10N75 10\\" Neodymium Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H7dc7672b996c4ba6ae4c746dd97f2551X.png"
-  },
-  {
-    "slug": "hd18sf130",
-    "title": "HD18SF130 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd18nw100",
-    "title": "HD18NW100 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd18n125a",
-    "title": "HD18N125A 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/Hc84ff75905d949a1bb39f8eb71f5e759t.jpg"
-  },
-  {
-    "slug": "hd18sf135",
-    "title": "HD18SF135 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hdpd1852-2",
-    "title": "HD/PD1852-2 18\\" Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hde-1080",
-    "title": "HDE-1080 Neodymium HF Driver",
-    "image": "https://s.alicdn.com/@sc04/kf/Hf54cbec419ce409f9d6dd2b4201a06b2m.jpg"
-  },
-  {
-    "slug": "hd18n455",
-    "title": "HD18N455 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hdw-1880",
-    "title": "HDW-1880 18\\" Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hdcx1275-75",
-    "title": "HDCX1275-75 12\\" Coaxial Speaker",
-    "image": "https://s.alicdn.com/@sc04/kf/H691c71da65be45ea80b1d36bc406ed18Y.jpg"
-  },
-  {
-    "slug": "hd21nl152",
-    "title": "HD21NL152 21\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H82cd13efe7be4425bc0b3037e9f52d5ar.jpg"
-  },
-  {
-    "slug": "subwoofer-18",
-    "title": "18 Inch High-Power Subwoofer",
-    "image": ""
-  },
-  {
-    "slug": "hd21sf2802",
-    "title": "HD21SF2802 21\\" Ferrite Subwoofer",
-    "image": "ALIBABA_IMAGE_URL_OR_PLACEHOLDER"
-  },
-  {
-    "slug": "hd15nl400c",
-    "title": "HD15NL400C 15\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/Hfdeb7c97246f4c289e9d539223ba06fdH.jpg"
-  },
-  {
-    "slug": "hd15g401",
-    "title": "HD15G401 15\\" Ferrite Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/He41fb50c244343ad992900535e53debcH.png"
-  },
-  {
-    "slug": "hde-914",
-    "title": "HDE-914 Neodymium HF Driver",
-    "image": "https://s.alicdn.com/@sc04/kf/Hcdea117476054b87870ab8eedad688f8j.jpg"
-  },
-  {
-    "slug": "hd18tbx100",
-    "title": "HD18TBX100 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd21fc330",
-    "title": "HD21FC330 21\\" Ferrite Subwoofer",
-    "image": "ALIBABA_IMAGE_URL_OR_PLACEHOLDER"
-  },
-  {
-    "slug": "hd21n125a",
-    "title": "HD21N125A 21\\" Neodymium Subwoofer",
-    "image": "ALIBABA_IMAGE_URL_OR_PLACEHOLDER"
-  },
-  {
-    "slug": "hdnk-1000",
-    "title": "HDNK-1000 Neodymium HF Driver",
-    "image": "https://s.alicdn.com/@sc04/kf/H0103de543a04459cb6d8d94f50c12f0eh.jpg"
-  },
-  {
-    "slug": "hd12nc402",
-    "title": "HD12NC402 12\\" Neodymium Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1c616559e4b94104942626480aa96a57s.png"
-  },
-  {
-    "slug": "hdpd1845",
-    "title": "HD/PD1845 18\\" Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd21sf330-2",
-    "title": "HD21SF330-2 21\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H04c3e74274c44908add23802070638U.jpg"
-  },
-  {
-    "slug": "hd18nc152a",
-    "title": "HD18NC152A 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1f98b0f1b98f48f6a325ebfbdeb46490P.png"
-  },
-  {
-    "slug": "hd18tbw100",
-    "title": "HD18TBW100 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd21n150a",
-    "title": "HD21N150A 21\\" Neodymium Subwoofer",
-    "image": "ALIBABA_IMAGE_URL_OR_PLACEHOLDER"
-  },
-  {
-    "slug": "hd18x401",
-    "title": "HD18X401 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd21nc152",
-    "title": "HD21NC152 21\\" Neodymium Subwoofer",
-    "image": "https://sc04.alicdn.com/kf/He3b8e453bc6e40b6b61b2e2fe434c40by/21-Speaker-Low-Frequency-DJ-Big-Power.jpg"
-  },
-  {
-    "slug": "hd18sw100",
-    "title": "HD18SW100 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd21nc152a",
-    "title": "HD21NC152A 21\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H01b9c3a7a12c4306b901375cd4e00bcbs.jpg"
-  },
-  {
-    "slug": "hd18sf330",
-    "title": "HD18SF330 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd15sf100",
-    "title": "HD15SF100 15\\" Ferrite Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H954484c1f4f24c78be9a5cb95693b3d6M.jpg"
-  },
-  {
-    "slug": "hd18x451",
-    "title": "HD18X451 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H4597dc9b82b346d4b7c62a740320f198S.jpg"
-  },
-  {
-    "slug": "hd15n401",
-    "title": "HD15N401 15\\" Neodymium Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H06d21a681c0d4453b343612f7202158dR.png"
-  },
-  {
-    "slug": "hd18sw125",
-    "title": "HD18SW125 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H698bea7bae2b49d084fcde55ac46d260n.jpg"
-  },
-  {
-    "slug": "hd18g401",
-    "title": "HD18G401 18\\" Ferrite Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H8294f2bba734450dafc5367034411b83W.png"
-  },
-  {
-    "slug": "hd08n50-16",
-    "title": "HD08N50-16 8\\" Neodymium Woofer",
-    "image": "https://s.alicdn.com/@sc04/kf/Hcea1485f8a724101b30748c3e9cd7dd9j.png"
-  },
-  {
-    "slug": "18sw1352",
-    "title": "18SW1352 18\\" Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd18x455",
-    "title": "HD18X455 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  },
-  {
-    "slug": "hd18sw115",
-    "title": "HD18SW115 18\\" Neodymium Subwoofer",
-    "image": "https://s.alicdn.com/@sc04/kf/H1d3fee8f30104d258eeb9f8901968673Q.jpg"
-  }
-];
+    {
+      "slug": "08n12",
+      "title": "08N12 Woofer Speaker Bass Professional Subwoofer 8 Inch Speaker",
+      "image": "https://sc02.alicdn.com/kf/H4cdbc222344e4c63b9fdc3070565eddfr.jpg"
+    },
+    {
+      "slug": "10nw64",
+      "title": "10NW64 10\" Professional Neodymium Woofer Speaker LF Driver",
+      "image": "https://sc02.alicdn.com/kf/Afb7d5b1759ef4b4aa74d643284859062K.png"
+    },
+    {
+      "slug": "12ndl76",
+      "title": "12NDL76 800w Neodymium Line Array Woofer Speaker 12 Inch Subwoofer",
+      "image": "https://sc02.alicdn.com/kf/Af79f5df638b24911a87c60587caf53b8B.png"
+    },
+    {
+      "slug": "15tbx100",
+      "title": "15TBX100 15\" Professional LF Driver Bass Speaker 2000W Max",
+      "image": "https://sc02.alicdn.com/kf/A36d0f7cd77a84d39acc2da4ad4636e4a8.png"
+    },
+    {
+      "slug": "18tbx100",
+      "title": "18TBX100 18\" Professional Subwoofer Bass Speaker 2400W Peak",
+      "image": "https://sc02.alicdn.com/kf/A78ae9eb6e6874da09992e68402a8180fQ.png"
+    },
+    {
+      "slug": "21nc152",
+      "title": "21NC152 21\" High End Neodymium Subwoofer 4000W Max",
+      "image": "https://sc02.alicdn.com/kf/H3c86bdd1e83048df9ad37e95d1c66404l.jpg"
+    }
+  ];
 
   return (
     <main className="bg-white py-24">
